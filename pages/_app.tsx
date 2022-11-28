8f4c873aa23450da1170ada82a2d3530c88621cd
johan_apo <johelapo@gmail.com>
2022-11-28 14:40:30 -0500

    Code for pageview added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import ReactGA from "react-ga";
 
 ReactGA.initialize("G-FTN0MS7BB0");
+ReactGA.pageview(window.location.pathname + window.location.search);
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
