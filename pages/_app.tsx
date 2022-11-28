cb47899d04f407c51de2051476b42f7522402578
johan_apo <johelapo@gmail.com>
2022-11-28 17:35:44 -0500

    Added useEffect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,36 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import ReactGA from "react-ga";
+import Script from "next/script";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
-ReactGA.initialize("G-FTN0MS7BB0");
-ReactGA.pageview(window.location.pathname + window.location.search);
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  useEffect(() => {
+    ReactGA.initialize("G-FTN0MS7BB0");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+    router.events.on("routeChangeComplete", () => console.log("Changed!"));
+    return () => router.events.off("routeChangeStart", () => console.log("Yo"));
+  }, []);
+
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-FTN0MS7BB0"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-FTN0MS7BB0');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,10 @@
-import Script from "next/script";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-FTN0MS7BB0"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-FTN0MS7BB0');
-        `}
-      </Script>
       <p>Hello</p>
+      <Link href="/threejs">Go to Threejs</Link>
     </main>
   );
 }
